247b1218bd15f51aa7a09fc2cb314411817d1423
Iron <perts.pasha.pro@gmail.com>
2020-07-07 18:15:22 +0300

    Lot adds

--- app/src/main/java/com/example/mononotes/activities/MainActivity.java
+++ app/src/main/java/com/example/mononotes/activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mononotes.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -12,14 +15,20 @@ import android.widget.ImageView;
 
 import com.example.mononotes.R;
 import com.example.mononotes.activities.CreateNoteActivity;
+import com.example.mononotes.adapters.NoteAdapter;
 import com.example.mononotes.database.NotesDataBase;
 import com.example.mononotes.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    private RecyclerView notesRecyclerView;
+    private List<Note> noteList;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        noteList = new ArrayList<>();
+        noteAdapter = new NoteAdapter(noteList);
+        notesRecyclerView.setAdapter(noteAdapter);
+
         getNotes();
     }
 
@@ -46,7 +63,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("MY_NOTES", notes.toString());
+                if (noteList.size() == 0){
+                    noteList.addAll(notes);
+                    noteAdapter.notifyDataSetChanged();
+                } else {
+                    noteList.add(0, notes.get(0));
+                    noteAdapter.notifyItemInserted(0);
+                }
+                notesRecyclerView.smoothScrollToPosition(0);
             }
 
             @Override
@@ -58,5 +82,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         new GetNotesTask().execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK){
+            getNotes();
+        }
     }
 }
--- app/src/main/java/com/example/mononotes/adapters/NoteAdapter.java
+++ app/src/main/java/com/example/mononotes/adapters/NoteAdapter.java
@@ -1,4 +1,67 @@
 package com.example.mononotes.adapters;
 
-public class NoteAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mononotes.R;
+import com.example.mononotes.entities.Note;
+
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+
+    private List<Note> notes;
+
+    public NoteAdapter(List<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NoteViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_container_note, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.setNote(notes.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textTitle, textSubtitle, textDataTime;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textSubtitle = itemView.findViewById(R.id.textSubtitle);
+            textDataTime = itemView.findViewById(R.id.textDataTimeHolder);
+        }
+
+        void setNote(Note note) {
+            textTitle.setText(note.getTitle());
+            if (note.getSubtitle().trim().isEmpty()) {
+                textSubtitle.setVisibility(View.GONE);
+            } else {
+                textSubtitle.setText(note.getSubtitle());
+            }
+            textDataTime.setText(note.getDateTime());
+        }
+    }
 }
